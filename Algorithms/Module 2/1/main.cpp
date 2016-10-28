59b7f42f0a6818d7afc75023a589c62584d00211
Ed Asriyan <ed-asriyan@protonmail.com>
2016-10-28 22:46:47 +0300

    Create ComparableString type

--- Algorithms/Module 2/1/main.cpp
+++ Algorithms/Module 2/1/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
 #include <string>
 
+class ComparableString : public std::string {
+	public:
+		bool operator<(const ComparableString& s) const;
+		bool operator>(const ComparableString& s) const;
+		bool operator<=(const ComparableString& s) const;
+		bool operator>=(const ComparableString& s) const;
+		bool operator==(const ComparableString& s) const;
+};
+
+bool ComparableString::operator<(const ComparableString& s) const {
+	const_iterator it1 = begin();
+	const_iterator it2 = s.begin();
+
+	const_iterator it1_end = end();
+	const_iterator it2_end = s.end();
+
+	while (it1 != it1_end && it2 != it2_end) {
+		if (*(it1++) < *(it2++)) {
+			return true;
+		} else if (*it1 > *it2) {
+			return false;
+		}
+	}
+
+	return size() < s.size();
+}
+
+bool ComparableString::operator>(const ComparableString& s) const {
+	return s.operator<(*this);
+}
+
+bool ComparableString::operator<=(const ComparableString& s) const {
+	return !s.operator<(*this);
+}
+
+bool ComparableString::operator>=(const ComparableString& s) const {
+	return !operator<(s);
+}
+
+bool ComparableString::operator==(const ComparableString& s) const {
+	return !operator<(s) && !s.operator<(*this);
+}
+
 /**
  * Sorts elements of the array by insertion method
  * @param array Pointer to the first object of the array to be sorted
@@ -27,7 +70,7 @@ int main() {
 	int n = 0;
 	std::cin >> n;
 
-	std::string* lines = new std::string[n];
+	ComparableString* lines = new ComparableString[n];
 	for (int i = 0; i < n; ++i) {
 		std::getline(std::cin, lines[i]);
 		std::cin >> lines[i];
