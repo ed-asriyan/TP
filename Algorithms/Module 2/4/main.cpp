4479063e274a6c7837b61119d68003caa2a26876
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-22 00:34:05 +0300

    Create algorithm wrapper

--- Algorithms/Module 2/4/main.cpp
+++ Algorithms/Module 2/4/main.cpp
@@ -7,7 +7,42 @@
 
 #include <iostream>
 
+/**
+ * @brief Splits the sequence to two parts.
+ * @param begin Iterator to the initial position in the sequence.
+ * @param end Iterator to the final position in the sequence.
+ * @return Iterator to the pivot element.
+ */
+template<class ITERATOR>
+ITERATOR Partition(ITERATOR begin, ITERATOR end) {
+	// todo: implement
+	return begin;
+}
+
+/**
+ * @brief calculates K statistics in the sequence.
+ * @param begin Iterator to the initial position in the sequence.
+ * @param end Iterator to the final position in the sequence.
+ * @return Value on the k-th place in the sorted sequence.
+ */
+template<class ITERATOR>
+auto CalcStatistics(ITERATOR begin, ITERATOR end, decltype(*begin) k) -> decltype(*begin) {
+	// todo: implement
+	return *begin;
+}
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	int n, k;
+	std::cin >> n >> k;
+
+	int* data = new int[n];
+	for (int i = 0; i < n; ++i) {
+		std::cin >> data[i];
+	}
+
+	std::cout << CalcStatistics(data, data + n, k);
+
+	delete[] data;
+
 	return 0;
 }
