fbc59537a5fe63bdfd86328f84e8c035ac415f74
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-23 02:12:12 +0300

    Implement the solution

--- Algorithms/Module 2/4/main.cpp
+++ Algorithms/Module 2/4/main.cpp
@@ -5,18 +5,54 @@
  * прохода двумя итераторами от конца массива к началу.
  */
 
+#include <random>
+#include <ctime>
 #include <iostream>
+#include <algorithm>
+
+/**
+ * @brief Generates random integer in range from min to max.
+ * @param min Lower border.
+ * @param max Highter border.
+ * @return Random integer in range from min to max.
+ */
+int GetRandom(int min, int max) {
+	return std::rand() % (max - min + 1) + min;
+
+	// the following code raises runtime error on the server :(
+//	std::random_device rd;
+//	std::mt19937 e2(rd());
+//	std::uniform_int_distribution<> dist(min, max);
+//	return dist(e2);
+}
 
 /**
  * @brief Splits the sequence to two parts.
  * @param begin Iterator to the initial position in the sequence.
  * @param end Iterator to the final position in the sequence.
- * @return Iterator to the pivot element.
+ * @return Iterator to the pivot position in the sequence.
  */
 template<class ITERATOR>
 ITERATOR Partition(ITERATOR begin, ITERATOR end) {
-	// todo: implement
-	return begin;
+	if (begin == end) {
+		return begin;
+	}
+
+	auto pivot = begin + GetRandom(0, static_cast<int>(std::distance(begin, end)) - 1);
+	std::iter_swap(begin, pivot);
+
+	auto it = end - 1;
+	auto jt = end - 1;
+	while (jt != begin) {
+		if (*jt >= *begin) {
+			std::iter_swap(it, jt);
+			--it;
+		}
+		--jt;
+	}
+	std::iter_swap(begin, it);
+
+	return it;
 }
 
 /**
@@ -26,12 +62,27 @@ ITERATOR Partition(ITERATOR begin, ITERATOR end) {
  * @return Value on the k-th place in the sorted sequence.
  */
 template<class ITERATOR>
-auto CalcStatistics(ITERATOR begin, ITERATOR end, decltype(*begin) k) -> decltype(*begin) {
-	// todo: implement
-	return *begin;
+auto CalcStatistics(ITERATOR begin, ITERATOR end, int k) -> decltype(*begin) {
+	auto first = begin;
+	auto last = end;
+
+	while (true) {
+		auto pivot = Partition(first, last);
+		int pivot_pos = pivot - begin;
+
+		if (pivot_pos == k) {
+			return *(begin + k);
+		} else if (pivot_pos > k) {
+			last = pivot;
+		} else {
+			first = pivot + 1;
+		}
+	}
 }
 
 int main() {
+	std::srand(static_cast<unsigned int>(std::time(NULL)));
+
 	int n, k;
 	std::cin >> n >> k;
 
@@ -46,3 +97,4 @@ int main() {
 
 	return 0;
 }
+
