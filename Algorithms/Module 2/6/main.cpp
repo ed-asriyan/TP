9ffc1dd9c16ee6fa7dca8575f11b1d9aa552c01f
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-23 02:41:55 +0300

    Create algorithm wrapper

--- Algorithms/Module 2/6/main.cpp
+++ Algorithms/Module 2/6/main.cpp
@@ -7,8 +7,57 @@
  */
 
 #include <iostream>
+#include <cassert>
+
+template<class T>
+void CountSort(T* arr, int n, int bit) {
+
+
+	T output[20];
+	int i, count[10] = {0};
+	for (i = 0; i < n; i++)
+		count[(arr[i] / bit) % 10]++;
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	for (i = n - 1; i >= 0; i--) {
+		output[count[(arr[i] / bit) % 10] - 1] = arr[i];
+		count[(arr[i] / bit) % 10]--;
+	}
+	for (i = 0; i < n; i++)
+		arr[i] = output[i];
+};
+
+/**
+ * @brief Sorts the sequence by radix (LSD).
+ * @param begin Iterator to the initial position in the sequence.
+ * @param end Iterator to the initial position in the sequence.
+ */
+template<class ITERATOR>
+void LsdSort(ITERATOR* begin, ITERATOR* end) {
+
+	assert(sizeof(*begin) <= sizeof(1ULL));
+
+	for (auto i = sizeof(*begin) - 1; i >= 0; --i) {
+		CountSort(begin, count, i);
+	}
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	size_t n;
+	std::cin >> n;
+
+	unsigned long long* array = new unsigned long long[n];
+	for (size_t i = 0; i < n; ++i) {
+		std::cin >> array[i];
+	}
+
+	LsdSort(array, n);
+
+	for (size_t i = 0; i < n; ++i) {
+		std::cout << array[i] << ' ';
+	}
+
+	delete[] array;
+
 	return 0;
 }
