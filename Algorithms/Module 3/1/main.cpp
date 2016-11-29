a19a6d78a6cb502203e777026b0e95d69f43522a
Ed Asriyan <ed-asriyan@protonmail.com>
2016-11-29 14:13:56 +0300

    Initial commit

---
+++ Algorithms/Module 3/1/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file
+ *
+ * 1_2.​ Для разрешения коллизий используйте двойное хеширование.
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
