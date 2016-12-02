f97709112059862d2933a96310df72a69a77e1f9
Ed Asriyan <ed-asriyan@protonmail.com>
2016-12-02 17:38:19 +0300

    Initial commit

---
+++ Algorithms/Module 3/2/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file
+ *
+ * 2_2. Выведите элементы в порядке pre-order (сверху вниз).
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
