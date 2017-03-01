4bf15356d418132c48861cf010938111f82b33b3
Ed Asriyan <ed-asriyan@protonmail.com>
2017-03-01 13:25:47 +0300

    Add the 15th task solution

---
+++ Database/HM 1/15.sql
@@ -0,0 +1,14 @@
+WITH sorted AS (
+    SELECT
+      movie.title    AS title,
+      movie.director AS director
+    FROM movie
+    ORDER BY movie.title
+)
+SELECT
+  string_agg(sorted.title, ',' ORDER BY sorted.title),
+  sorted.director
+FROM sorted
+GROUP BY sorted.director
+HAVING count(*) > 1
+ORDER BY sorted.director;
